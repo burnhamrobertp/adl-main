14f69417c1d50655d37100410a8d7696cccc2f15
Robert Burnham <burnhamrobertp@gmail.com>
2017-03-27 01:22:11 -0500

    Add basic react components

---
+++ resources/assets/js/moduleList.js
@@ -0,0 +1,33 @@
+import React from 'react';
+
+import Filters from './filters';
+
+class ModuleList extends React.Component {
+    render() {
+        return (
+            <div className="container">
+                <div className="row equalH">
+                    <div className="col equalH">
+                        <Filters />
+                    </div>
+
+                    <div className="col-8 equalH">
+                        <div id="adl-adventures">
+                            <div className="p-3 row">
+                                <div className="col">
+                                    <label htmlFor="adl-search-sortby">Sort By: </label>
+                                    <select id="adl-search-sortby">
+                                        <option>Rating</option>
+                                    </select>
+                                </div>
+                                <div id="adl-search-summary" className="col float-right text-right">9001 Adventures Found</div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        )
+    }
+}
+
+export default ModuleList;
--- resources/views/default.blade.php
+++ resources/views/default.blade.php
@@ -6,13 +6,12 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
     <title>Adventure Lookup</title>
-
-    <link rel="stylesheet" href="css/app.css"/>
-    <script type="text/javascript" src="js/commons.js"></script>
-    <script type="text/javascript" src="js/app.js"></script>
 </head>
 <body>
-    @include('layout.navbar')
-    @include($template)
+
+<div id="root"></div>
+<script type="text/javascript" src="js/commons.js"></script>
+<script type="text/javascript" src="js/app.js"></script>
+
 </body>
 </html>
