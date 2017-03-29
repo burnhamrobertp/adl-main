990555523abfef4693355814bc94202a67bd26ac
Robert Burnham <burnhamrobertp@gmail.com>
2017-03-29 00:48:27 -0500

    Rename viewmodels to scenes

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -3,7 +3,7 @@ import React from 'react'
 import ReactDOM from 'react-dom';
 import {BrowserRouter as Router, Route} from 'react-router-dom'
 
-import ModuleList from './viewmodels/moduleList/moduleList'
+import ModuleList from './scenes/moduleList/moduleList'
 import Navbar from './components/navbar/navbar'
 
 const App = () => (
