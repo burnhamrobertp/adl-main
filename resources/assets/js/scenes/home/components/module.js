e837a1e020df83fc128f82f9ae5c5e15dff918a6
Robert Burnham <burnhamrobertp@gmail.com>
2017-03-29 23:45:07 -0500

    Add actual module content to the modulesList

--- resources/assets/js/scenes/home/components/module.js
+++ resources/assets/js/scenes/home/components/module.js
@@ -3,9 +3,32 @@ import React from 'react'
 class Module extends React.Component {
     render() {
         const module = this.props.data;
+        let level = '',
+            length = '';
+
+        if (module.min_level && module.max_level) {
+            level = <span>| Levels {module.min_level} - {module.max_level}</span>
+        }
+        if (module.length) {
+            length = <span>| {module.length.name}</span>
+        }
+
         return (
-            <div className="p-2 row">
-                <div className="col">{module.name}</div>
+            <div className="module">
+                <div className="line"></div>
+                <div className="p-2 row">
+                    <div className="module-header col-8">
+                        <h5>{module.name}</h5>
+                        <div className="module-subheader">{module.edition.name} {level} {length}</div>
+                        <div className="module-summary">{module.summary}</div>
+                    </div>
+                    <div className="col-1">
+                        Rating
+                    </div>
+                    <div className="col">
+                        Image
+                    </div>
+                </div>
             </div>
         )
     }
