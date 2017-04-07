7df4b90118041676984e0cde3b7d37602de67c2a
Robert Burnham <burnhamrobertp@gmail.com>
2017-04-07 07:54:47 -0500

    Change active style for buttons to match old scss

--- resources/assets/js/scenes/home/components/atoms/Button.js
+++ resources/assets/js/scenes/home/components/atoms/Button.js
@@ -1,33 +1,29 @@
 import React from 'react';
 
 const DEFAULT = {
-    color        : '#444',
-    borderRadius : '4px',
+
 };
 
 const ACTIVE = {
-    color        : 'white',
-    borderRadius : '4px',
-    background   : 'rgb(66, 184, 221)'
-}
+    background: '#ffce3a'
+};
 
 class Button extends React.Component {
-    
     getStyle() {
-        if( this.props.active ) {
+        if (this.props.active) {
             return ACTIVE;
         }
-        
+
         return DEFAULT;
     }
-    
+
     click() {
-        this.props.click( this.props.name, this.props.activeListings );
+        this.props.click(this.props.name, this.props.activeListings);
     }
-    
+
     render() {
         return (
-            <button style={this.getStyle()} onClick={this.click.bind( this )}>
+            <button style={this.getStyle()} onClick={this.click.bind(this)}>
                 {this.props.name}
             </button>
         );
