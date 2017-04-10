98f1dfbf4035313e77ff3ebcc9ca3b8bdbf72184
Robert Burnham <burnhamrobertp@gmail.com>
2017-04-10 08:56:05 -0500

    Slight modifications for LoginController, simplify api routes file

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -4,6 +4,8 @@ namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Foundation\Auth\AuthenticatesUsers;
+use Illuminate\Http\Request;
+use Lcobucci\JWT\Builder;
 
 class LoginController extends Controller
 {
@@ -15,5 +17,34 @@ class LoginController extends Controller
     public function __construct()
     {
         $this->middleware('guest', ['except' => 'logout']);
+    }
+
+    protected function validateLogin(Request $request)
+    {
+        $this->validate($request, [
+            $this->username() => 'required', 'password' => 'required',
+        ]);
+    }
+
+    /**
+     * Attempts login, returns token(s) if successful
+     *
+     * @param Request $request
+     */
+    public function login(Request $request)
+    {
+        $this->validateLogin($request);
+
+        $success = $this->attemptLogin($request);
+
+        if ($success) {
+            $token = $this->generateToken($request);
+        } else {
+
+        }
+    }
+
+    protected function generateToken(Request $request)
+    {
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -1,81 +1,30 @@
 <?php
 
-Route::group([ 'prefix' => 'contributors' ], function() {
-    Route::get('', 'ContributorsController@index');
-    Route::get('{contributor}', 'ContributorsController@get');
-    Route::post('', 'ContributorsController@store');
-    Route::post('{contributor}', 'ContributorsController@update');
-    Route::delete('{contributor}', 'ContributorsController@destroy');
-});
+$crudRoutes = [
+    ['prefix' => 'contributors', 'parameter' => 'contributor', 'controller' => 'ContributorsController'],
+    ['prefix' => 'contributorTypes', 'parameter' => 'contributorType', 'controller' => 'ContributorTypesController'],
+    ['prefix' => 'creatures', 'parameter' => 'creature', 'controller' => 'CreaturesController'],
+    ['prefix' => 'creatureTypes', 'parameter' => 'creatureType', 'controller' => 'CreatureTypesController'],
+    ['prefix' => 'editions', 'parameter' => 'edition', 'controller' => 'EditionsController'],
+    ['prefix' => 'items', 'parameter' => 'item', 'controller' => 'ItemsControlller'],
+    ['prefix' => 'moduleLengths', 'parameter' => 'moduleLength', 'controller' => 'ModuleLengthsController'],
+    ['prefix' => 'modules', 'parameter' => 'module', 'controller' => 'ModulesController'],
+    ['prefix' => 'publishers', 'parameter' => 'publisher', 'controller' => 'PublishersController'],
+    ['prefix' => 'settings', 'parameter' => 'setting', 'controller' => 'SettingsController'],
+];
 
-Route::group([ 'prefix' => 'contributorTypes' ], function() {
-    Route::get('', 'ContributorTypesController@index');
-    Route::get('{contributorType}', 'ContributorTypesController@get');
-    Route::post('', 'ContributorTypesController@store');
-    Route::post('{contributorType}', 'ContributorTypesController@update');
-    Route::delete('{contributorType}', 'ContributorTypesController@destroy');
-});
-
-Route::group([ 'prefix' => 'creatures' ], function() {
-    Route::get('', 'CreaturesController@index');
-    Route::get('{creature}', 'CreaturesController@get');
-    Route::post('', 'CreaturesController@store');
-    Route::post('{creature}', 'CreaturesController@update');
-    Route::delete('{creature}', 'CreaturesController@destroy');
-});
-
-Route::group([ 'prefix' => 'creatureTypes' ], function() {
-    Route::get('', 'CreatureTypesController@index');
-    Route::get('{creatureType}', 'CreatureTypesController@get');
-    Route::post('', 'CreatureTypesController@store');
-    Route::post('{creatureType}', 'CreatureTypesController@update');
-    Route::delete('{creatureType}', 'CreatureTypesController@destroy');
-});
-
-Route::group([ 'prefix' => 'editions' ], function() {
-    Route::get('', 'EditionsController@index');
-    Route::get('{edition}', 'EditionsController@get');
-    Route::post('', 'EditionsController@store');
-    Route::post('{edition}', 'EditionsController@update');
-    Route::delete('{edition}', 'EditionsController@destroy');
-});
-
-Route::group([ 'prefix' => 'items' ], function() {
-    Route::get('', 'ItemsController@index');
-    Route::get('{item}', 'ItemsController@get');
-    Route::post('', 'ItemsController@store');
-    Route::post('{item}', 'ItemsController@update');
-    Route::delete('{item}', 'ItemsController@destroy');
-});
-
-Route::group([ 'prefix' => 'moduleLengths' ], function() {
-    Route::get('', 'ModuleLengthsController@index');
-    Route::get('{moduleLength}', 'ModuleLengthsController@get');
-    Route::post('', 'ModuleLengthsController@store');
-    Route::post('{moduleLength}', 'ModuleLengthsController@update');
-    Route::delete('{moduleLength}', 'ModuleLengthsController@destroy');
-});
-
-Route::group([ 'prefix' => 'modules' ], function() {
-    Route::get('', 'ModulesController@index');
-    Route::get('{module}', 'ModulesController@get');
-    Route::post('', 'ModulesController@store');
-    Route::post('{module}', 'ModulesController@update');
-    Route::delete('{module}', 'ModulesController@destroy');
-});
-
-Route::group([ 'prefix' => 'publishers' ], function() {
-    Route::get('', 'PublishersController@index');
-    Route::get('{publisher}', 'PublishersController@get');
-    Route::post('', 'PublishersController@store');
-    Route::post('{publisher}', 'PublishersController@update');
-    Route::delete('{publisher}', 'PublishersController@destroy');
-});
-
-Route::group([ 'prefix' => 'settings' ], function() {
-    Route::get('', 'SettingsController@index');
-    Route::get('{setting}', 'SettingsController@get');
-    Route::post('', 'SettingsController@store');
-    Route::post('{setting}', 'SettingsController@update');
-    Route::delete('{setting}', 'SettingsController@destroy');
+Route::group(['middleware' => ['api']], function () use ($crudRoutes) {
+    Route::group(['prefix' => 'auth'], function () {
+        Route::post('', 'Auth\LoginController@login');
+    });
+    
+    foreach ($crudRoutes as $route) {
+        Route::group(['prefix' => $route['prefix']], function() use ($route) {
+            Route::get("", "{$route['controller']}@index");
+            Route::get("{{$route['parameter']}}", "{$route['controller']}@get");
+            Route::post("", "{$route['controller']}@store");
+            Route::post("{{$route['parameter']}}", "{$route['controller']}@update");
+            Route::delete("{{$route['parameter']}}", "{$route['controller']}@destroy");
+        });
+    }
 });
