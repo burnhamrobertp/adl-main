f3013ea65a6484e7b915a7dc0fef0b1366a7a6b2
Robert Burnham <burnhamrobertp@gmail.com>
2017-04-11 21:28:50 -0500

    Almost pointlessly simple module page

--- resources/assets/js/Scenes/Home/Components/Module.jsx
+++ resources/assets/js/Scenes/Home/Components/Module.jsx
@@ -1,35 +1,49 @@
 import React from 'react'
+import {Link} from 'react-router-dom'
 
 class Module extends React.Component {
+    moduleLink() {
+        return '/module/'+this.props.data.id;
+    }
+
+    renderLevel() {
+        const level = module.min_level && module.max_level
+                ? <span>| Levels {module.min_level} - {module.max_level}</span>
+                : '';
+
+        return level;
+    }
+
+    renderLength() {
+        const length = module.length
+            ? <span>| {module.length.name}</span>
+            : '';
+
+        return length;
+    }
+
     render() {
         const module = this.props.data;
-        let level = '',
-            length = '';
-
-        if (module.min_level && module.max_level) {
-            level = <span>| Levels {module.min_level} - {module.max_level}</span>
-        }
-        if (module.length) {
-            length = <span>| {module.length.name}</span>
-        }
 
         return (
-            <div className="module">
-                <div className="line"></div>
-                <div className="p-2 row">
-                    <div className="module-header col-8">
-                        <h5>{module.name}</h5>
-                        <div className="module-subheader">{module.edition.name} {level} {length}</div>
-                        <div className="module-summary">{module.summary}</div>
-                    </div>
-                    <div className="col-1">
-                        Rating
-                    </div>
-                    <div className="col">
-                        Image
+            <Link to={this.moduleLink()}>
+                <div className="module">
+                    <div className="line"/>
+                    <div className="p-2 row">
+                        <div className="module-header col-8">
+                            <h5>{module.name}</h5>
+                            <div className="module-subheader">{module.edition.name} {this.renderLevel()} {this.renderLength()}</div>
+                            <div className="module-summary">{module.summary}</div>
+                        </div>
+                        <div className="col-1">
+                            Rating
+                        </div>
+                        <div className="col">
+                            Image
+                        </div>
                     </div>
                 </div>
-            </div>
+            </Link>
         )
     }
 }
---
+++ resources/assets/js/Scenes/Module/Module.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+
+class Module extends React.Component {
+    render() {
+        return (
+            <div className="container">
+                <div className="row">
+                    Its a module!
+                </div>
+            </div>
+        )
+    }
+}
+
+export default Module;
